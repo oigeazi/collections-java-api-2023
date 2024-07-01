53a068cf13d0b9ddc0ebfb64f6e33a46fe27ac03
Geazi <oigeazi@gmail.com>
2024-07-01 17:12:35 -0300

    Carrinho de Compras

---
+++ List/src/OperacoesBasicas/Item.java
@@ -0,0 +1,35 @@
+package OperacoesBasicas;
+
+public class Item {
+
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public Item(String nome, Double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+
+}
